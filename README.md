fde205d6984d060ce45651ee615b5e6cdde0d3ad
BucekJiri <jibu@nodesagency.com>
2022-10-17 15:38:33 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NotificationsClient
+
+A description of this package.
---
+++ Sources/NotificationsClient/NotificationsClient.swift
@@ -0,0 +1,6 @@
+public struct NotificationsClient {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
